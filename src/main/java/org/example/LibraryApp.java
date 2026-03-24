da4acbe7216827f2a58d097b7172ab814bd2a3a7
mintOcaTshy <sofia.marinina@student.karazin.ua>
2026-03-24 18:58:02 +0200

    third modify

--- src/main/java/org/example/LibraryApp.java
+++ src/main/java/org/example/LibraryApp.java
@@ -3,142 +3,148 @@ package org.example;
 import java.io.*;
 import java.util.*;
 
-class Human {
+class Human implements Externalizable {
     String firstName;
     String lastName;
+
+    public Human() {}
+
     public Human(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(firstName);
+        out.writeObject(lastName);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        firstName = (String) in.readObject();
+        lastName = (String) in.readObject();
+    }
+
+    @Override
+    public String toString() { return firstName + " " + lastName; }
 }
 
 class Author extends Human {
+    public Author() { super(); }
     public Author(String firstName, String lastName) { super(firstName, lastName); }
 }
 
-class Book {
+class Book implements Externalizable {
     String title;
     int year;
-    List<Author> authors;
+    List<Author> authors = new ArrayList<>();
+
+    public Book() {}
+
     public Book(String title, int year, List<Author> authors) {
         this.title = title;
         this.year = year;
         this.authors = authors;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(title);
+        out.writeInt(year);
+        out.writeObject(authors);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        title = (String) in.readObject();
+        year = in.readInt();
+        authors = (List<Author>) in.readObject();
     }
 }
 
 class BookReader extends Human {
     int readerId;
     List<Book> borrowedBooks = new ArrayList<>();
+
+    public BookReader() { super(); }
+
     public BookReader(String firstName, String lastName, int readerId) {
         super(firstName, lastName);
         this.readerId = readerId;
     }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        super.writeExternal(out);
+        out.writeInt(readerId);
+        out.writeObject(borrowedBooks);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        super.readExternal(in);
+        readerId = in.readInt();
+        borrowedBooks = (List<Book>) in.readObject();
+    }
 }
 
-class Library implements Serializable {
-    private static final long serialVersionUID = 2L;
+class Library implements Externalizable {
     String name;
+    List<Book> allBooks = new ArrayList<>();
+    List<BookReader> allReaders = new ArrayList<>();
 
-    transient List<Book> allBooks = new ArrayList<>();
-    transient List<BookReader> allReaders = new ArrayList<>();
+    public Library() {}
 
     public Library(String name) { this.name = name; }
 
-    private void writeObject(ObjectOutputStream out) throws IOException {
-        out.defaultWriteObject();
-
-        out.writeInt(allBooks.size());
-        for (Book b : allBooks) {
-            saveBookData(out, b);
-        }
-
-        out.writeInt(allReaders.size());
-        for (BookReader r : allReaders) {
-            out.writeObject(r.firstName);
-            out.writeObject(r.lastName);
-            out.writeInt(r.readerId);
-
-            out.writeInt(r.borrowedBooks.size());
-            for (Book b : r.borrowedBooks) {
-                saveBookData(out, b);
-            }
-        }
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(name);
+        out.writeObject(allBooks);
+        out.writeObject(allReaders);
     }
 
-    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        in.defaultReadObject();
-        int bookCount = in.readInt();
-        allBooks = new ArrayList<>();
-        for (int i = 0; i < bookCount; i++) {
-            allBooks.add(loadBookData(in));
-        }
-
-        int readerCount = in.readInt();
-        allReaders = new ArrayList<>();
-        for (int i = 0; i < readerCount; i++) {
-            BookReader r = new BookReader((String)in.readObject(), (String)in.readObject(), in.readInt());
-            int borrowedCount = in.readInt();
-            for (int j = 0; j < borrowedCount; j++) {
-                r.borrowedBooks.add(loadBookData(in));
-            }
-            allReaders.add(r);
-        }
-    }
-
-    private void saveBookData(ObjectOutputStream out, Book b) throws IOException {
-        out.writeObject(b.title);
-        out.writeInt(b.year);
-        out.writeInt(b.authors.size());
-        for (Author a : b.authors) {
-            out.writeObject(a.firstName);
-            out.writeObject(a.lastName);
-        }
-    }
-
-    private Book loadBookData(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        String title = (String) in.readObject();
-        int year = in.readInt();
-        int authCount = in.readInt();
-        List<Author> authors = new ArrayList<>();
-        for (int i = 0; i < authCount; i++) {
-            authors.add(new Author((String)in.readObject(), (String)in.readObject()));
-        }
-        return new Book(title, year, authors);
+    @SuppressWarnings("unchecked")
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        name = (String) in.readObject();
+        allBooks = (List<Book>) in.readObject();
+        allReaders = (List<BookReader>) in.readObject();
     }
 
     @Override
     public String toString() {
-        return "Бібліотека (v2): " + name + ", Книг: " + allBooks.size() + ", Читачів: " + allReaders.size();
+        return "Бібліотека: " + name + " (Книг: " + allBooks.size() + ", Читачів: " + allReaders.size() + ")";
     }
 }
 
 public class LibraryApp {
     public static void main(String[] args) {
-        Library myLibrary = new Library("Одеська японська бібліотека");
+        Library myLibrary = new Library("Токійський книжковий сад");
 
         Author murakami = new Author("Харукі", "Муракамі");
-        Book book1 = new Book("Норвезький ліс", 1987, Collections.singletonList(murakami));
-        myLibrary.allBooks.add(book1);
+        Book book = new Book("Норвезький ліс", 1987, Collections.singletonList(murakami));
+        myLibrary.allBooks.add(book);
 
-        BookReader reader = new BookReader("Наталя", "Петренко", 445);
-        reader.borrowedBooks.add(book1);
+        BookReader reader = new BookReader("Олексій", "Іванов", 101);
+        reader.borrowedBooks.add(book);
         myLibrary.allReaders.add(reader);
 
-        String filename = "library_custom.ser";
+        String filename = "library_v3.ext";
 
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
             out.writeObject(myLibrary);
-            System.out.println("/// Успішно збережено у " + filename + " ///");
+            System.out.println("/// Дані успішно експортовано ///");
         } catch (IOException e) { e.printStackTrace(); }
 
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
             Library loaded = (Library) in.readObject();
-            System.out.println("\n/// Дані відновлено! ///");
+            System.out.println("\n/// Дані відновлено ///");
             System.out.println(loaded);
-
-            BookReader restoredReader = loaded.allReaders.get(0);
-            System.out.println("Читач: " + restoredReader.firstName + " тримає книгу: " + restoredReader.borrowedBooks.get(0).title);
+            System.out.println("Читач " + loaded.allReaders.get(0).firstName + " читає: " + loaded.allReaders.get(0).borrowedBooks.get(0).title);
         } catch (Exception e) { e.printStackTrace(); }
     }
 }
